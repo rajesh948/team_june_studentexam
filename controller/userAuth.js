4f65f790e6570e6996609f900c7e9a84ae00a1ed
Sumit-Bhardwaj <sumit.bhardwaj.eSparkbiz@gmail.com>
2023-03-24 09:12:42 +0530

    23dev056 clearing session cookie after logout and edit profile working

--- controller/userAuth.js
+++ controller/userAuth.js
@@ -120,7 +120,7 @@ const login_api = async (req, res) => {
 
   if (check_pass) {
     req.session.user_id = data[0].user_id;
-    req.session.stud_id = studdata[0].id;
+    req.session.stud_id = studdata[0].student_id;
     req.session.email = login_data.email;
     res.redirect("/home");
   } else {
@@ -234,11 +234,13 @@ const updatedata = async (req, res) => {
 
   var session = req.session;
 
+  console.log(session)
+
   var user_id = session.user_id;
   var stud_id = session.stud_id;
 
   try {
-    var sql = `update student_master set fname='${alldata.fname}', lname='${alldata.lname}', gender='${alldata.gender}', email='${alldata.email}', mobile='${alldata.phone}', enrollment='${alldata.enroll}', qualification='${alldata.qualification}', city='${alldata.city}', college='${alldata.college}', birthdate='${alldata.dob}' where id=${stud_id}`;
+    var sql = `update student_master set fname='${alldata.fname}', lname='${alldata.lname}', gender='${alldata.gender}', email='${alldata.email}', mobile='${alldata.phone}', enrollment='${alldata.enroll}', qualification='${alldata.qualification}', city='${alldata.city}', college='${alldata.college}', birthdate='${alldata.dob}' where student_id=${stud_id}`;
     var [result1] = await con.query(sql);
 
     var sql1 = `update user_master set username='${alldata.email}' where user_id=${user_id}`;
