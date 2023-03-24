234a27d0cc859359a8a6ebdee7821ef96834405e
Sumit-Bhardwaj <sumit.bhardwaj.eSparkbiz@gmail.com>
2023-03-24 09:59:15 +0530

    23dev056 home exam rendering with resume button

--- controller/userAuth.js
+++ controller/userAuth.js
@@ -145,7 +145,7 @@ const home = async (req, res) => {
     var sql1 = `select exam_name,exam_id from exam_master where exam_isActive="yes"`;
     var [examdata] = await con.query(sql1);
 
-    var sql2 = `select exam_name from exam_master,result_master where exam_master.exam_id=result_master.exam_id and submited = "1" and exam_isActive="yes" and user_id=${user_id}`;
+    var sql2 = `select exam_name,submited from exam_master,result_master where exam_master.exam_id=result_master.exam_id and exam_isActive="yes" and user_id=${user_id}`;
     var [attemptdata] = await con.query(sql2);
 
     console.log(examdata)
@@ -157,11 +157,24 @@ const home = async (req, res) => {
       flag = 0;
       for (let j = 0; j < attemptdata.length; j++) {
         if (examdata[i].exam_name == attemptdata[j].exam_name) {
-          data.push({
-            exam_id: examdata[i].exam_id,
-            exam_name: examdata[i].exam_name,
-            attempted: true,
-          });
+          if(attemptdata[j].submited==1)
+          {
+            data.push({
+              exam_id: examdata[i].exam_id,
+              exam_name: examdata[i].exam_name,
+              attempted: true,
+              giving: false
+            });
+          }
+          if(attemptdata[j].submited==0)
+          {
+            data.push({
+              exam_id: examdata[i].exam_id,
+              exam_name: examdata[i].exam_name,
+              attempted: false,
+              giving: true
+            });
+          }
           flag = 1;
         }
       }
@@ -170,9 +183,12 @@ const home = async (req, res) => {
           exam_id: examdata[i].exam_id,
           exam_name: examdata[i].exam_name,
           attempted: false,
+          giving: false,
         });
       }
     }
+
+    console.log(data);
 
     res.render("home", { data, username });
   } else {
